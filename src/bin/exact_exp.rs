e6deaffd9f1204f6d145697988e417439cd7b5aa
Timon Behr <timon.behr@uni-konstanz.de>
2022-08-16 22:39:33 +0200

    added to exact

--- src/bin/exact_exp.rs
+++ src/bin/exact_exp.rs
@@ -89,6 +89,7 @@ pub fn main() -> Result<(), Box<dyn error::Error>> {
 
     // Initialize output files
     let mut out_files = vec![
+        File::create(format!("{}/sr.csv",dest))?,
         File::create(format!("{}/link.csv",dest))?,
         File::create(format!("{}/twins.csv",dest))?,
         File::create(format!("{}/dome.csv",dest))?,
@@ -103,46 +104,49 @@ pub fn main() -> Result<(), Box<dyn error::Error>> {
         File::create(format!("{}/all_but_petal.csv",dest))?,
     ];
     writeln!(&mut out_files[0], "name, nk, mk, sk,\
-             t_st, n_st, m_st,\
-             t_ln, n_ln, m_ln")?;
+             t_st, n_st, m_st")?;
 
     writeln!(&mut out_files[1], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
-             t_tn, n_tn, m_tn")?;
+             t_ln, n_ln, m_ln")?;
 
     writeln!(&mut out_files[2], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
-             t_dome, n_dome, m_dome")?;
+             t_tn, n_tn, m_tn")?;
 
     writeln!(&mut out_files[3], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
-             t_cliq, n_cliq, m_cliq")?;
+             t_dome, n_dome, m_dome")?;
 
     writeln!(&mut out_files[4], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
-             t_core, n_core, m_core")?;
+             t_cliq, n_cliq, m_cliq")?;
 
     writeln!(&mut out_files[5], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
-             t_domino, n_domino, m_domino")?;
+             t_core, n_core, m_core")?;
 
     writeln!(&mut out_files[6], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
-             t_scc, n_scc, m_scc")?;
+             t_domino, n_domino, m_domino")?;
 
     writeln!(&mut out_files[7], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
+             t_scc, n_scc, m_scc")?;
+
+    writeln!(&mut out_files[8], "name, nk, mk, sk,\
+             t_st, n_st, m_st,\
              t_ap, n_ap, m_ap")?;
     
-    writeln!(&mut out_files[8], "name, nk, mk, sk,\
+    writeln!(&mut out_files[9], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
              t_qap, n_qap, m_qap")?;
 
-    writeln!(&mut out_files[9], "name, nk, mk, sk,\
+    writeln!(&mut out_files[10], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
              t_p, n_p, m_p")?;
 
-    writeln!(&mut out_files[10], "name, nk, mk, sk,\
+    writeln!(&mut out_files[11], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
              t_ln, n_ln, m_ln,\
              t_tn, n_tn, m_tn,\
@@ -152,7 +156,7 @@ pub fn main() -> Result<(), Box<dyn error::Error>> {
              t_scc, n_scc, m_scc,\
              t_ap, n_ap, m_ap")?;
 
-    writeln!(&mut out_files[11], "name, nk, mk, sk,\
+    writeln!(&mut out_files[12], "name, nk, mk, sk,\
              t_st, n_st, m_st,\
              t_ln, n_ln, m_ln,\
              t_tn, n_tn, m_tn,\
@@ -199,7 +203,7 @@ pub fn main() -> Result<(), Box<dyn error::Error>> {
     }
 
     for (dfvsi_org, name) in instances {
-        for p in 0..12 {
+        for p in 0..13 {
             let (start_sender, start_receiver) = channel();
             let (interrupt_sender, interrupt_receiver) = channel();
             let (done_sender, done_receiver) = channel();
@@ -330,7 +334,11 @@ fn write_simple_empty(
     g_name: &OsString, out_file: &mut File, go: usize) -> Result<(), Box<dyn error::Error>> {
     let mut line = String::new();
     if go < 10 {
-        line.push_str(&format!("{:?},,,,,,,,,", g_name));
+        if go == 0 {
+            line.push_str(&format!("{:?},,,,,,", g_name));
+        } else {
+            line.push_str(&format!("{:?},,,,,,,,,", g_name));
+        }
     } else {
         line.push_str(&format!("{:?},,,,,,,,,,,,,,,,,,,,,,,,,,,", g_name));
     }
